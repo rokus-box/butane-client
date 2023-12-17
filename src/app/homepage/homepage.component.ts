857755f3d3615dab65f4ebd1808f87af68801b7f
Oguz Turkay <tyserract@gmail.com>
2023-12-17 20:36:48 +0300

    fix: make responsive

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent {
-
+  isMobile = window.innerWidth < 768;
 }
